adadef9c809d9aac1139c47515b9321265b7d34d
PurpleFlow22 <110283369+PurpleFlow22@users.noreply.github.com>
2023-10-23 13:32:10 +0200

    Updated feature dictionary and bug fixed

--- data_process.py
+++ data_process.py
@@ -1,4 +1,7 @@
 import csv
+import json
+import os.path
+import string
 
 
 def process_dataset(file_path='adult/adult.data'):
@@ -8,6 +11,7 @@ def process_dataset(file_path='adult/adult.data'):
     train_label = map(lambda row: row[-1], data)
     return list(train_data), list(train_label), feature_dict, continuous_features, category_features
 
+
 def clean_data(file_path):
     # read the training data as 2d list
     with open(file_path, newline='') as csvfile:
@@ -15,10 +19,10 @@ def clean_data(file_path):
     if file_path.find('.test') != -1:
         data = data[1:len(data)-1]  # remove first text row
     else:
-        data = data[:len(data)-1]  # There is a white space before each string. Strip white space
-    print("The size of the data: ", len(data), len(data[0]))
-    for i in range(len(data)):  # There is a white space before each string. Strip white space
-        data[i] = [s.strip() for s in data[i]]
+        data = data[:len(data)-1]  # There is a blank line in the end
+    print("The size of the data before preprocessing: ", len(data), len(data[0]))
+    for i in range(len(data)):  # There is a white space before each string. Strip white space and remove dot
+        data[i] = [s.strip().removesuffix('.') for s in data[i]]
     cleaned = [row for row in data if not any(el == '?' for el in row)]  # remove entries that contain "?"
     [row.pop(13) for row in cleaned]  # remove the 'native-country' column
     print("The size of the data after preprocessing: ", len(cleaned), len(cleaned[0]))
@@ -45,6 +49,7 @@ def replace(my_list, lookup_dict):
                     break
     return res
 
+
 # convert a row to readable form based on the input feature dictionary
 def make_readable(row, f_dict):
     readable = []
@@ -52,7 +57,8 @@ def make_readable(row, f_dict):
         readable.append(f_dict[i][row[i]])
     return readable
 
-def encode(cleaned):
+
+def encode(cleaned, file_path='my_dict.json'):
     # colnames = ['age','workclass','fnlwgt','education','education-num','marital-status',
     #             'occupation','relationship','race','sex','capital-gain','capital-loss',
     #             'hours-per-week','income']
@@ -63,34 +69,52 @@ def encode(cleaned):
     for i in continuous_features:  # convert continuous values from string to float
         columns[i] = [float(x) for x in columns[i]]
 
+    if not os.path.exists(file_path):
+        print("Feature dictionary not found. Creating new...")
     # create lookup dictionary for categorical and continuous features
-    category_dict = []
-    continuous_dict=[]
-    for i in category_features:
-        lookup_values = set(columns[i])
-        category_dict.append(dict(zip(range(len(lookup_values)),lookup_values)))
-    # age: <=20,21-30,31-40,41-50,51-60,61+
-    continuous_dict.append(dict(zip(range(6),['20','30','40','50','60','60+'])))
-    # fnlwgt(in 1e6): <=0.1, 0.1-0.2, ..., 0.6+
-    continuous_dict.append(dict(zip(range(7),['100000','200000','300000','400000','500000','600000','600000+'])))
-    # education-num: <=8, 8-9, 9-10, ..., 15-16
-    continuous_dict.append(dict(zip(range(9),['8','9','10','11','12','13','14','15','16'])))
-    # captital-gain: <=5000, 5000-10000, 10000-15000, 15000-20000, 20000+
-    continuous_dict.append(dict(zip(range(5),['5000','10000','15000','20000','20000+'])))
-    # capital-loss: <=1000, 1000-1500, 1500-2000, 2000+
-    continuous_dict.append(dict(zip(range(4),['1000','1500','2000','2000+'])))
-    # hours-per-week: <=10, 10-20, 20-30, 30-40, 40-50, 50-60, 60+
-    continuous_dict.append(dict(zip(range(7),['10','20','30','40','50','60','60+'])))
+        category_dict = []
+        continuous_dict=[]
+        for i in category_features:
+            lookup_values = set(columns[i])
+            category_dict.append(dict(zip(range(len(lookup_values)),lookup_values)))
+        # age: <=20,21-30,31-40,41-50,51-60,61+
+        # continuous_dict.append(dict(zip(range(6),['20','30','40','50','60','60+'])))
+        continuous_dict.append(dict(zip(range(5), ['30', '40', '50', '60', '60+'])))
+        # fnlwgt(in 1e6): <=0.1, 0.1-0.2, ..., 0.6+
+        # continuous_dict.append(dict(zip(range(7),['100000','200000','300000','400000','500000','600000','600000+'])))
+        continuous_dict.append(dict(zip(range(2), ['600000', '600000+'])))
+        # education-num: <=8, 8-9, 9-10, ..., 15-16
+        # continuous_dict.append(dict(zip(range(9),['8','9','10','11','12','13','14','15','16'])))
+        continuous_dict.append(dict(zip(range(3), ['11', '15', '15+'])))
+        # captital-gain: <=5000, 5000-10000, 10000-15000, 15000-20000, 20000+
+        continuous_dict.append(dict(zip(range(5),['5000','10000','15000','20000','20000+'])))
+        # capital-loss: <=1000, 1000-1500, 1500-2000, 2000+
+        continuous_dict.append(dict(zip(range(4),['1000','1500','2000','2000+'])))
+        # hours-per-week: <=10, 10-20, 20-30, 30-40, 40-50, 50-60, 60+
+        # continuous_dict.append(dict(zip(range(7),['10','20','30','40','50','60','60+'])))
+        continuous_dict.append(dict(zip(range(3), ['20', '40', '40+'])))
 
-    # create full list of feature dictionary, transform data accordingly
-    all_features = continuous_features+category_features
-    all_dict = continuous_dict+category_dict
-    feature_dict = [d for idx,d in sorted(zip(all_features, all_dict))]
+        # create full list of feature dictionary, transform data accordingly
+        all_features = continuous_features+category_features
+        all_dict = continuous_dict+category_dict
+        feature_dict = [d for idx, d in sorted(zip(all_features, all_dict))]
+
+        with open('my_dict.json', 'w') as f:  # save the feature dictionary
+            json.dump(feature_dict, f)
+        print("Feature dictionary is created.")
+    else:
+        print("Found existing feature dictionary.")
+        with open('my_dict.json') as f:
+            feature_dict = json.load(f)
+        for i in range(len(feature_dict)):  # convert keys from str to int
+            feature_dict[i] = {int(k): v for k, v in feature_dict[i].items()}
+
     for i in range(len(feature_dict)):
         columns[i] = replace(columns[i], feature_dict[i])
     backToRows = list(zip(*columns))  # transpose columns back to rows
     backToRows = [list(row) for row in backToRows]
-    print("Feature dictionary is created. Data ready to use.")
+
+    print("Data ready to use.")
     # example
     # print("Data size: \n", len(backToRows), len(backToRows[0]))
     # print("Data before transform: \n", cleaned[0])
@@ -102,4 +126,3 @@ def encode(cleaned):
 
 if __name__ == '__main__':
     train_data, train_label, feature_dict, continuous_features, category_features = process_dataset()
-    print(feature_dict)
